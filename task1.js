e04da83e683a30005c169aad578715c24442bae6
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2022-11-08 13:26:33 +0000

    explanation on task 1 and 2

--- task1.js
+++ task1.js
@@ -3,6 +3,18 @@ const cats = ["tony", "daisy", "socks", "rockie" ];
 // a function that takes in a string and turns it to uppercase
 // .map() method to generate a new array
 
+
+// Here we create a variable ("capitalisedCats") and assing it to 
+// whatever comes back from the .map array method.
+
+/* The .map method consists in creating an anonymous function 
+ that takes one argument, we named it "cat" but it's value will
+ come from the iteration of each item on "cats" array, defined at the top*/
+
+/*After that we need only to transform each item iterated to 
+uppercase and return it, doing that the .map method will create
+another array with the values it found, beyond storing it into 
+the capitalizedCats variable */ 
 const capitalisedCats = cats.map((cat) => {
     return cat.toUpperCase();
 });
--- task2.js
+++ task2.js
@@ -7,7 +7,13 @@ let animals = [
     "horse",
     "basalisk",
   ];
-  
+
+/* The .filter is similar to the .map method but instead of changing 
+each item of the designated array it will just filter them out based 
+on the condition we give, after that it will also return a new array
+no matter what.
+*/
+
 let bListAnimals = animals.filter((animal) => {
   // Your 'filter' code here.
   //.filter() method to generate a new array 
